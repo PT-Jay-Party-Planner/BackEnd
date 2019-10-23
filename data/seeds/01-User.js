0a5b8ceb06b78f17adce39046efbf4697c1ac8a8
Jay Maas <jaymaas.dev@gmail.com>
2019-10-22 20:27:48 -0600

    Seed edits

--- data/seeds/01-User.js
+++ data/seeds/01-User.js
@@ -4,9 +4,13 @@ exports.seed = function(knex, Promise) {
 	const password = bcrypt.hashSync('1234', 10);
 	return knex('users').del().then(function() {
 		return knex('users').insert([
-			{ id: 1, username: 'Ian', password: password },
-			{ id: 2, username: 'Elisa', password: password },
-			{ id: 3, username: 'Josh', password: password }
+			{ username: 'Jay', password: password },
+			{ username: 'Colby', password: password },
+			{ username: 'Ryan', password: password },
+			{ username: 'Michelle', password: password },
+			{ username: 'Courtney', password: password },
+			{ username: 'Pete', password: password },
+			{ username: 'Tauan', password: password },
 		]);
 	});
 };
